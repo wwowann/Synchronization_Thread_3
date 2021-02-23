4ea970661e08a93877df213a6272f9af9f48b26a
”wwowann” <ivasin75@mail.ru>
2021-02-23 23:19:34 +0000

    add all

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,8 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         final int MAX = 5;
-       Restaurant restaurant = new Restaurant();
-        new Thread(null, restaurant::cookCooks, "Абдулрахим").start();
+        Restaurant restaurant = new Restaurant();
+        new Thread(null,restaurant::cookCooks, "Абдулрахим").start();
         message("Повар ", "Абдулрахим");
         new Thread(null, restaurant::getOrder, "Вася").start();
         message("Официант ", "Вася");
@@ -17,15 +17,15 @@ public class Main {
             System.out.println("Посетитель " + (i + 1) + " пришел в ресторан");
             Random random = new Random();
             try {
-                Thread.sleep(random.nextInt(5 * 1000));
+                Thread.sleep(random.nextInt(10 * 1000));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-
     }
 
     public static void message(String name, String name1) {
-        System.out.println(name +  name1 + " на работе");
+        System.out.println(name +
+                name1 + " на работе");
     }
 }
--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -1,80 +1,98 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Restaurant {
-    int MAX = 5;
+    final int MAX = 5;
+    private int counterDish = 0;
     List<OrderWaiter> orderWaiters = new ArrayList<>(10);
     List<OrderCook> orderCooks = new ArrayList<>(10);
     List<ReadyDish> readyDish = new ArrayList<>(10);
-
-    final Object customerWaiter = new Object();
-    final Object waiterCook = new Object();
-    final Object waiterCustomer = new Object();
+    private final ReentrantLock lock = new ReentrantLock();
+    private final ReentrantLock lock1 = new ReentrantLock();
+    private final ReentrantLock lock2 = new ReentrantLock();
+    private final Condition customerWaiter = lock2.newCondition();
+    private final Condition waiter = lock.newCondition();
+    private final Condition waiterCook = lock1.newCondition();
 
     public void getOrder() {
-        int count = 0;
-        while (count < MAX) {
-            synchronized (customerWaiter) {
-                if (orderWaiters.size() == 0) {
-                    try {
-                        System.out.println(Thread.currentThread().getName() +
-                                " свободен и ждет от посетителя сигала");
-                        customerWaiter.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-            synchronized (waiterCook) {
-                orderCooks.add(new OrderCook());
+        while (counterDish < MAX) {
+            lock.lock();
+            if (orderWaiters.size() == 0 || readyDish.size() == 0) {
                 System.out.println(Thread.currentThread().getName() +
-                        " взял заказ и отнес к повару");
-                if (readyDish.size() == 0) {
+                        " свободен и ждет от посетителя или повара сигнала ");
+                try {
                     try {
-                        waiterCook.notify();// сигнал повару готовить
-                        System.out.println(Thread.currentThread().getName() +
-                                " ждет блюдо от повара");
-                        waiterCook.wait();
+                        waiter.await();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
+                    if (orderWaiters.size() > 0) {
+                        lock1.lock();
+                        try {
+                            orderCooks.add(new OrderCook());
+                            System.out.println(Thread.currentThread().getName() + " принял заказ у посетителя " +
+                                    "и отнес повару для приготовления блюда");
+                            try {
+                                Thread.sleep(2000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            orderWaiters.remove(0);
+                            waiterCook.signal();
+                        } finally {
+                            lock1.unlock();
+                        }
+                    }
+                    if (readyDish.size() > 0) {
+                        lock2.lock();
+                        try {
+                            System.out.println(Thread.currentThread().getName() + " забрал блюдо у повара " +
+                                    "и отнес посетителю");
+                            System.out.println("counterDish " + (1 + counterDish++));
+                            readyDish.remove(0);
+                            customerWaiter.signal();
+                        } finally {
+                            lock2.unlock();
+                        }
+                    }
+                } finally {
+                    lock.unlock();
                 }
-                System.out.println(Thread.currentThread().getName() + " отнес блюдо посетителю");
-                synchronized (waiterCustomer) {
-                    readyDish.remove(0);
-                    waiterCustomer.notify();
-                }
-
             }
-            count++;
+        }
+        try {
+            Thread.currentThread().join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
     public void customer() {
-        synchronized (customerWaiter) {
+        System.out.println(Thread.currentThread().getName() +
+                " зовет официанта и делает заказ");
+        lock.lock();
+        try {
             orderWaiters.add(new OrderWaiter());
-            try {
-                Thread.sleep(3000);
-                System.out.println(Thread.currentThread().getName() +
-                        " зовет официанта и делает заказ");
-                customerWaiter.notify();
-                orderWaiters.remove(0);
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            Thread.sleep(2000);
+            waiter.signal();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
         }
         System.out.println(Thread.currentThread().getName() +
                 " ждет блюдо");
-        synchronized (waiterCustomer) {
-            try {
-                waiterCustomer.wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        lock2.lock();
+        try {
+            customerWaiter.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock2.unlock();
         }
-
         System.out.println(Thread.currentThread().getName() + " приступил к еде");
         Random random = new Random();
         try {
@@ -87,30 +105,43 @@ public class Restaurant {
 
     public void cookCooks() {
         int count = 0;
-        while (count < MAX) {
-            synchronized (waiterCook) {
-                if (orderCooks.size() == 0) {
-                    try {
-                        waiterCook.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+        while (count < 5) {
+            lock1.lock();
+            if (orderCooks.size() == 0) {
+                try {
+                    waiterCook.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock1.unlock();
                 }
-                System.out.println(Thread.currentThread().getName() + " готовит блюдо");
+            }
+
+            System.out.println(Thread.currentThread().getName() + " принял заказ и готовит блюдо");
+            lock.lock();
+            try {
                 readyDish.add(new ReadyDish());
                 try {
-                    Thread.sleep(3000);
-                    System.out.println("блюдо готово");
-                    waiterCook.notify();
-                    System.out.println(Thread.currentThread().getName() + " ждет заказ");
-                    waiterCook.wait();
-
+                    Thread.sleep(5000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                System.out.println(Thread.currentThread().getName() + " приготовил блюдо");
+                orderCooks.remove(0);
+                count++;
+                waiter.signal();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
             }
-            count++;
         }
+        System.out.println("Повар " + Thread.currentThread().getName() +
+                " приготовил 5 блюд и закончил свою работу");
     }
 }
+
+
+
+
 
